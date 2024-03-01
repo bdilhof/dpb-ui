29b416fcb2c890afc2970b45592712dfb0f4e891
Bruno Dilhof <brunodilhof@gmail.com>
2024-03-01 13:19:23 +0100

    Update flash messages component

--- resources/views/flash-messages.blade.php
+++ resources/views/flash-messages.blade.php
@@ -1,5 +1,23 @@
-@if(session('status-success'))
+@if(session('success'))
     <div class="alert alert-success m-0" role="alert">
-        {{ session('status-success') }}
+        {{ session('success') }}
+    </div>
+@endif
+
+@if(session('warning'))
+    <div class="alert alert-warning m-0" role="alert">
+        {{ session('warning') }}
+    </div>
+@endif
+
+@if(session('danger'))
+    <div class="alert alert-danger m-0" role="alert">
+        {{ session('danger') }}
+    </div>
+@endif
+
+@if(session('info'))
+    <div class="alert alert-info m-0" role="alert">
+        {{ session('info') }}
     </div>
 @endif
