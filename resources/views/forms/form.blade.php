39cac2ad37bb4935c627d3fbf31569b4c8293d2c
Bruno Dilhof <brunodilhof@gmail.com>
2024-03-04 11:31:36 +0100

    Update form component

--- resources/views/forms/form.blade.php
+++ resources/views/forms/form.blade.php
@@ -1,5 +1,16 @@
-<form {{ $attributes }}>
+@props(['method' => 'POST'])
+
+@php
+    $method = strtoupper($method);
+    $formMethod = in_array($method, ['GET', 'POST']) ? $method : 'POST';
+@endphp
+
+<form method="{{ $formMethod === 'GET' ? 'GET' : 'POST' }}" action="{{ $attributes->get('action') }}" {{ $attributes }}>
     @csrf
+
+    @if (in_array($method, ['PUT', 'PATCH', 'DELETE']))
+        @method($method)
+    @endif
 
     <div class="vstack gap-4">
         <div class="vstack gap-3">
