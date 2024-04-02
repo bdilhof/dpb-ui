9bf5bfc8a0bcc323f258f5d7f53f7d185575d4cd
Bruno Dilhof <brunodilhof@gmail.com>
2024-04-02 14:06:06 +0200

    Update flash-messages.blade.php

--- resources/views/flash-messages.blade.php
+++ resources/views/flash-messages.blade.php
@@ -1,23 +1,27 @@
 @if(session('success'))
-    <div class="alert alert-success m-0" role="alert">
+    <div class="alert alert-dismissible fade show alert-success m-0" role="alert">
         {{ session('success') }}
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>
 @endif
 
 @if(session('warning'))
-    <div class="alert alert-warning m-0" role="alert">
+    <div class="alert alert-dismissible fade show alert-warning m-0" role="alert">
         {{ session('warning') }}
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>
 @endif
 
 @if(session('danger'))
-    <div class="alert alert-danger m-0" role="alert">
+    <div class="alert alert-dismissible fade show alert-danger m-0" role="alert">
         {{ session('danger') }}
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>
 @endif
 
 @if(session('info'))
-    <div class="alert alert-info m-0" role="alert">
+    <div class="alert alert-dismissible fade show alert-info m-0" role="alert">
         {{ session('info') }}
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>
 @endif
