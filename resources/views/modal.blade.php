d35687a59a5d9b0dce7b6cbc8728b540574762d7
Bruno Dilhof <brunodilhof@gmail.com>
2024-04-05 13:51:55 +0200

    Update modal.blade.php

--- resources/views/modal.blade.php
+++ resources/views/modal.blade.php
@@ -2,22 +2,28 @@
     "color" => "secondary"
 ])
 
-<div class="modal fade" tabindex="-1" data-bs-backdrop="static" {{ $attributes }}>
+<div class="modal fade" tabindex="-1" {{ $attributes }}>
   <div class="modal-dialog modal-dialog-centered">
     <div class="modal-content text-wrap">
 
+      @if(isset($header))
       <div class="modal-header text-bg-{{ $color }}">
-        <h1 class="modal-title fs-5" id="exampleModalLabel">{{ $header }}</h1>
+        <h1 class="modal-title fs-5" id="exampleModalLabel">
+          {{ $header }}
+        </h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
+      @endif
 
       <div class="modal-body">
         {{ $slot }}
       </div>
 
+      @if(isset($footer))
       <div class="modal-footer">
         {{ $footer }}
       </div>
+      @endif
 
     </div>
   </div>
