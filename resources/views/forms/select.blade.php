5d89367a45c91c7e98c612191f99775ff5228fc6
Bruno Dilhof <brunodilhof@gmail.com>
2024-04-10 14:54:03 +0200

    Update select.blade.php

--- resources/views/forms/select.blade.php
+++ resources/views/forms/select.blade.php
@@ -2,20 +2,21 @@
     "name" => "",
     "items" => collect([]),
     "label" => "",
-    "value" => null
+    "value" => null,
+    "id" => "",
 ])
 
 <div>
 
     <!-- Label -->
     @if($label)
-    <label for="assigned_to" class="form-label">
+    <label for="{{ $id }}" class="form-label">
         {{ $label }}
     </label>
     @endif
 
     <!-- Select -->
-    <select {{ $attributes }} name="{{ $name }}" class="form-select">
+    <select {{ $attributes }} name="{{ $name }}" id="{{ $id }}" class="form-select">
         <option selected disabled>{{ trans('ui.choose') }}</option>
         @foreach($items as $item)
         <option @selected($item->value == $value) value="{{ $item->value }}">{{ $item->label }}</option>
