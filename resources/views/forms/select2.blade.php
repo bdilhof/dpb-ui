17618aa0bb79f1c2907aedf876d55aaf0874d678
Bruno Dilhof <brunodilhof@gmail.com>
2024-04-15 09:18:48 +0200

    Update select2.blade.php

--- resources/views/forms/select2.blade.php
+++ resources/views/forms/select2.blade.php
@@ -12,7 +12,7 @@
     <label class="form-label" for="{{ $id }}">{{ $label }}</label>
     @endif
 
-    <select data-placeholder="{{ trans('ui.choose') }}" name="{{ $name }}" id="{{ $id }}" @class(["js-select2", "is-invalid" => $errors->has($name)])>
+    <select data-placeholder="{{ trans('ui.choose') }}" name="{{ $name }}" id="{{ $id }}" {{ $attributes }} @class(["js-select2", "is-invalid" => $errors->has($name)])>
         <option @selected(!$value) disabled>{{ trans('ui.choose') }}</option>
         @foreach($options as $option)
         <option @selected($value == $option->value) value="{{ $option->value }}">{{ $option->label }}</option>           
