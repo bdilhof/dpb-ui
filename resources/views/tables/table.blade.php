a87f82f1648b3535e695a82902dded2973550952
Bruno Dilhof <brunodilhof@gmail.com>
2024-04-18 15:26:24 +0200

    Update table.blade.php

--- resources/views/tables/table.blade.php
+++ resources/views/tables/table.blade.php
@@ -1,14 +1,10 @@
-@props([
-    'small' => false,
-    'striped' => false,
-])
-
-<div class="card">
-    <div class="card-body">
-        <div class="table-responsive">
-            <table @class(['table align-middle text-nowrap m-0', 'table-sm' => $small, 'table-striped' => $striped, 'table-hover'])>
-                {{ $slot }}
-            </table>
-        </div>
-    </div>
+<div class="table-responsive">
+    <table {{ $attributes->merge(['class' => 'table table-hover align-middle table-bordered text-nowrap m-0']) }}>
+        <thead class="table-light">
+            {{ $thead }}
+        </thead>
+        <tbody>
+            {{ $tbody }}
+        </tbody>
+    </table>
 </div>
