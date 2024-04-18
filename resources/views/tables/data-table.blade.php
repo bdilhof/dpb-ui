e3f5b30f72cdd2b051c15bce69bf5088d29222aa
Bruno Dilhof <brunodilhof@gmail.com>
2024-04-18 15:30:00 +0200

    Update data-table.blade.php

--- resources/views/tables/data-table.blade.php
+++ resources/views/tables/data-table.blade.php
@@ -4,29 +4,27 @@
     "columnNames",
 ])
 
-<div class="table-responsive">
-    <table class="table align-middle text-nowrap m-0 table-striped">
-        <thead>
-            <tr>
-                @foreach($columns as $column)
-                <th>{{ $columnNames[$loop->index] ?? $column }}</th>
-                @endforeach
-            </tr>
-        </thead>
-        <tbody>
-            @forelse($items as $item)
-            <tr>
-                @foreach($columns as $column)
-                    <td>{{ $item->$column }}</td>
-                @endforeach
-            </tr>
-            @empty
-            <tr>
-                <td colspan="{{ count($columns) }}" class="text-center">
-                    {{ trans('ui.empty_table') }}
-                </td>
-            </tr>
-            @endforelse
-        </tbody>
-    </table>
-</div>
+<x-ui::tables.table>
+    <x-slot name="thead">
+        <tr>
+            @foreach($columns as $column)
+            <th>{{ $columnNames[$loop->index] ?? $column }}</th>
+            @endforeach
+        </tr>
+    </x-slot>
+    <x-slot name="tbody">
+        @forelse($items as $item)
+        <tr>
+            @foreach($columns as $column)
+            <td>{{ $item->$column }}</td>
+            @endforeach
+        </tr>
+        @empty
+        <tr>
+            <td colspan="{{ count($columns) }}" class="text-center">
+                {{ trans('ui.empty_table') }}
+            </td>
+        </tr>
+        @endforelse
+    </x-slot>
+</x-ui::tables.table>
