562c2470e98edbe0585027df1c7544ac6cefc06a
Bruno Dilhof <brunodilhof@gmail.com>
2024-05-03 12:09:43 +0200

    Update user-badge.blade.php

--- resources/views/user-badge.blade.php
+++ resources/views/user-badge.blade.php
@@ -1,11 +1,11 @@
 @props(["user", "color" => false])
 
 @if($color)
-<span style="background-color: {{ $user->color }} !important" class="badge rounded-pill text-uppercase text-bg-primary me-1" title="{{ $user->nameFormatted }}">
+<span style="background-color: {{ $user->color }} !important" class="user-badge" title="{{ $user->nameFormatted }}">
     {{ $user->initials }}
 </span>
 @else
-<span class="badge rounded-pill text-uppercase text-bg-secondary me-1" title="{{ $user->nameFormatted }}">
+<span class="user-badge" title="{{ $user->nameFormatted }}">
     {{ $user->initials }}
 </span>
 @endif
