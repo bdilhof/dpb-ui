9be7ee1d6709eb36c95c4888c9b4ef986d3370b8
Bruno Dilhof <brunodilhof@gmail.com>
2024-05-21 14:39:15 +0200

    Update table.blade.php

--- resources/views/tables/table.blade.php
+++ resources/views/tables/table.blade.php
@@ -1,10 +1,8 @@
-<div class="table-responsive">
-    <table {{ $attributes->merge(['class' => 'table table-hover align-middle table-bordered text-nowrap m-0']) }}>
-        <thead class="table-light">
-            {{ $thead }}
-        </thead>
-        <tbody>
-            {{ $tbody }}
-        </tbody>
-    </table>
-</div>
+<table {{ $attributes->merge(['class' => 'table table-hover align-middle table-bordered text-nowrap m-0']) }}>
+    <thead class="table-light">
+        {{ $thead }}
+    </thead>
+    <tbody>
+        {{ $tbody }}
+    </tbody>
+</table>
