b5bede2eded7aa169836174051bd379b7840379d
Bruno Dilhof <brunodilhof@gmail.com>
2024-07-02 15:45:35 +0200

    Remove stage notice from flash messages

--- resources/views/flash-messages.blade.php
+++ resources/views/flash-messages.blade.php
@@ -1,13 +1,3 @@
-<!-- Staging notice -->
-@env('staging')
-    <div class="alert alert-dismissible fade show alert-warning m-0" role="alert">
-        <h4 class="alert-heading">Testovacie prostredie</h4>
-        <p class="mb-0">Toto je testovacia verzia projektu, určená výlučne na prehliadanie a skúšanie nových funkcionalít pred ich nasadením do produkčného prostredia.</p>
-        <p class="mb-0">Prosím, nepoužívajte túto verziu na žiadne iné účely. Pre bežné používanie a živú prevádzku použite produkčnú verziu dostupnú na: <a class="alert-link" href="{{ config('app.url_production') }}" target="_blank">{{ config('app.url_production') }}</a></p>
-        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-    </div>
-@endenv
-
 @if(session('success'))
     <div class="alert alert-dismissible fade show alert-success m-0" role="alert">
         {{ session('success') }}
