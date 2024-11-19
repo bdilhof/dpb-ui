d137c57d8c34d5e3a30934d4c3a36ca604784c38
Bruno Dilhof <brunodilhof@gmail.com>
2024-11-19 14:55:04 +0100

    Update user-badge.blade.php

--- resources/views/user-badge.blade.php
+++ resources/views/user-badge.blade.php
@@ -4,12 +4,15 @@
     "size" => 26
 ])
 
-@if($color)
-<span style="width: {{ $size }}px; height: {{ $size }}px; line-height: {{ $size }}px; background-color: {{ $user->color }} !important;" class="user-badge bg-secondary" title="{{ $user->nameFormatted }}">
+<span 
+    class="user-badge bg-secondary" 
+    title="{{ $user->nameFormatted }}" 
+    @style([
+        "width: {$size}px",
+        "height: {$size}px",
+        "line-height: {$size}px",
+        $color ? "background-color: {$user->color} !important" : null,
+    ])
+>
     {{ $user->initials }}
 </span>
-@else
-<span style="width: {{ $size }}px; height: {{ $size }}px; line-height: {{ $size }}px;" class="user-badge bg-secondary" title="{{ $user->nameFormatted }}">
-    {{ $user->initials }}
-</span>
-@endif
